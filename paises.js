ad9e6dd5101c8b0d468914a0aa44187532b73690
nmtdavid <nmtdavid@gmail.com>
2020-11-30 09:12:19 -0400

    Create paises.js
    Agregando json de paises

---
+++ paises.js
@@ -0,0 +1,352 @@
+[
+    {
+        "id_pais": 1,
+        "nombre": "Estado Plurinacional de Bolivia",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 2,
+        "nombre": "Australia",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 3,
+        "nombre": "Austria",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 4,
+        "nombre": "Azerbaiyán",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 5,
+        "nombre": "Anguilla",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 6,
+        "nombre": "Argentina",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 7,
+        "nombre": "Armenia",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 8,
+        "nombre": "Bielorrusia",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 9,
+        "nombre": "Belice",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 10,
+        "nombre": "Bélgica",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 11,
+        "nombre": "Bermudas",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 12,
+        "nombre": "Bulgaria",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 13,
+        "nombre": "Brasil",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 14,
+        "nombre": "Reino Unido",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 15,
+        "nombre": "Hungría",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 16,
+        "nombre": "Vietnam",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 17,
+        "nombre": "Haiti",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 18,
+        "nombre": "Guadalupe",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 19,
+        "nombre": "Alemania",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 20,
+        "nombre": "Países Bajos, Holanda",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 21,
+        "nombre": "Grecia",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 22,
+        "nombre": "Georgia",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 23,
+        "nombre": "Dinamarca",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 24,
+        "nombre": "Egipto",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 25,
+        "nombre": "Israel",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 26,
+        "nombre": "India",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 27,
+        "nombre": "Irán",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 28,
+        "nombre": "Irlanda",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 29,
+        "nombre": "España",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 30,
+        "nombre": "Italia",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 31,
+        "nombre": "Kazajstán",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 32,
+        "nombre": "Camerún",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 33,
+        "nombre": "Canadá",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 34,
+        "nombre": "Chipre",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 35,
+        "nombre": "Kirguistán",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 36,
+        "nombre": "China",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 37,
+        "nombre": "Costa Rica",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 38,
+        "nombre": "Kuwait",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 39,
+        "nombre": "Letonia",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 40,
+        "nombre": "Libia",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 41,
+        "nombre": "Lituania",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 42,
+        "nombre": "Luxemburgo",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 43,
+        "nombre": "México",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 44,
+        "nombre": "Moldavia",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 45,
+        "nombre": "Mónaco",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 46,
+        "nombre": "Nueva Zelanda",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 47,
+        "nombre": "Noruega",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 48,
+        "nombre": "Polonia",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 49,
+        "nombre": "Portugal",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    },
+    {
+        "id_pais": 50,
+        "nombre": "Reunión",
+        "estado": "ACTIVO",
+        "fecha_creacion": "2020-11-30T13:04:45.302Z",
+        "fecha_modificacion": "2020-11-30T13:04:45.302Z"
+    }
+]
